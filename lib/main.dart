da3e5c724fe2dfdf4db9af0069e91ba60dfa03e3
basic-bhavya <bjain.nd@gmail.com>
2021-06-08 12:15:22 +0530

    reset

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:hey_look/login_page.dart';
-import 'package:hey_look/utils/constants.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'utils/constants.dart';
 
-import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 
-Future main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  Constants.prefs = await SharedPreferences.getInstance();
+  // Constants.prefs = await SharedPreferences.getInstance();
 
-  runApp(MaterialApp(
-      title: "Hey Look",
-      home: Constants.prefs?.getBool("loggedIn") == true
-          ? HomePage()
-          : LoginPage(),
-      theme: ThemeData(primarySwatch: Colors.blue),
-      routes: {
-        '/login': (context) => LoginPage(),
-        '/home': (context) => HomePage()
-      }));
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  TextEditingController _nameController = TextEditingController();
-  var myText = "Change This";
-  var url = "https://jsonplaceholder.typicode.com/photos";
-  var data;
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
-
-  getData() async {
-    var res = await http.get(Uri.parse(url));
-    data = jsonDecode(res.body);
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Something"),
-        actions: [
-          IconButton(
-              onPressed: () {
-                Constants.prefs?.setBool("loggedIn", false);
-                Navigator.pushReplacementNamed(context, "/login");
-              },
-              icon: Icon(Icons.logout))
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: data != null
-            ? ListView.builder(
-                itemCount: data.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListTile(
-                      title: Text(data[index]["title"]),
-                      subtitle: Text("${data[index]["id"]}"),
-                      leading: Image.network(data[index]["url"]),
-                    ),
-                  );
-                },
-              )
-            : Center(child: CircularProgressIndicator()),
-      ),
-      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          myText = _nameController.text;
-          setState(() {});
-        },
-        child: Icon(Icons.refresh),
-      ),
-    );
-  }
+  runApp(MaterialApp(title: "Hey Look", home: Text("Hello guys!")));
 }
