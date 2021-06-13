2dc681876288349e68a645d9e6e4f4b344834d63
basic-bhavya <bjain.nd@gmail.com>
2021-06-13 19:05:05 +0530

    getting data from objects

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,63 @@
 import 'package:flutter/material.dart';
-
-import 'quiz.dart';
-import 'result.dart';
-
-// import 'package:shared_preferences/shared_preferences.dart';
+import 'package:hey_look/transaction.dart';
 
 void main() {
-  runApp(QuizApp());
+  runApp(MyApp());
 }
 
-class QuizApp extends StatefulWidget {
-  @override
-  _QuizState createState() => _QuizState();
-}
-
-class _QuizState extends State<QuizApp> {
-  var _questionIndex = 0;
-  var _totalScore = 0;
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    const _questions = [
-      {
-        'questionText': 'What is food?',
-        'answers': [
-          {'fud': false},
-          {'fooood': true},
-          {'fodd': false}
-        ]
-      },
-      {
-        'questionText': 'What is life?',
-        'answers': [
-          {'42': true},
-          {'21': false},
-          {'9': false},
-          {'0': false}
-        ]
-      },
-      // {
-      //   'questionText': 'What is this?',
-      //   'answers': ['A smoothie', 'an app', 'spartan']
-      // }
-    ];
-
-    // const simpleMap = {'hi': 'bye', 'yes': 'no'};
-
-    // simpleMap.forEach((key, value) {
-    //   print(key);
-    // });
-
-    void _answerQuestion(int score) {
-      if (_questionIndex < _questions.length)
-        setState(() {
-          _questionIndex = _questionIndex + 1;
-        });
-
-      _totalScore += score;
-    }
-
     return MaterialApp(
-        title: "Hey Look",
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text("My App"),
-            ),
-            body: _questionIndex < _questions.length
-                ? Quiz(
-                    answerQuestion: _answerQuestion,
-                    questions: _questions,
-                    questionIndex: _questionIndex)
-                : Result(_totalScore)));
+      title: 'Flutter App',
+      home: ExpensePlannerApp(),
+    );
+  }
+}
+
+class ExpensePlannerApp extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: "New Shoes", price: 10000, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: "Weekly Groceries", price: 3000, date: DateTime.now())
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Expense Planner"),
+      ),
+      body: Column(
+        children: [
+          Card(
+            child: Text("Chart"),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: transactions
+                .map((tx) => Card(
+                      child: Row(
+                        children: [
+                          Container(
+                            child: Text(tx.price.toString()),
+                          ),
+                          Column(
+                            children: [
+                              Text(tx.title as String),
+                              Text(tx.date.toString())
+                            ],
+                          )
+                        ],
+                      ),
+                    ))
+                .toList(),
+          )
+        ],
+      ),
+    );
   }
 }
